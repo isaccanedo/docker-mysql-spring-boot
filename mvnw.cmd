5faaa7a98abfb5749ea8109e9601f48d485172eb
Isac Canedo <32867124+isaccanedo@users.noreply.github.com>
2021-09-08 08:23:29 -0300

    Update mvnw.cmd

--- mvnw.cmd
+++ mvnw.cmd
@@ -1,7 +1,7 @@
 @REM ----------------------------------------------------------------------------
 @REM Licenciado para a Apache Software Foundation (ASF) sob um 
-@REM ou mais contratos de licença de contribuidor.
-@REM or more contributor license agreements.  See the NOTICE file
+@REM ou mais contratos de licença de contribuidor
+@REM ou mais contratos de licença de contribuidor.  See the NOTICE file
 @REM distributed with this work for additional information
 @REM regarding copyright ownership.  The ASF licenses this file
 @REM to you under the Apache License, Version 2.0 (the
